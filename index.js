9c1da86d22c3f2402a54823ac3eaffa63ccfa440
angel <angeldrummer2000@gmail.com>
2024-07-19 06:43:09 -0700

    v2

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+
+const bodyParser = require('body-parser');
+const dns = require('dns');
+
+
+app.use(bodyParser.urlencoded({ extended: false })); //Not sure how the body parser methods work
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -18,6 +25,45 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+//In-memory storage for URLs
+let URLDatabase = [];
+let idCounter= 1;
+
+app.post('/api/shorturl', function(req, res){
+  let original_url = req.body.url //I am able to access this beacause of bodyParser
+
+  try {
+    const url = new URL(original_url);
+    const hostname = url.hostname;
+
+    //look up DNS to validate the url
+    dns.lookup(hostname, (err, address, family)=>{
+      if (err){
+        res.json({ error: 'invalid url' });
+      } else{
+        let shortUrl = idCounter++;
+        URLDatabase.push({ original_url: original_url, short_url: shortUrl})
+        res.json({ original_url: original_url, short_url: shortUrl});
+      }
+    });
+  } catch (e) {
+    res.json({ error: 'invalid url' })
+  }
+});
+
+app.get('/api/shorturl/:short_url', function(req, res){
+  let shortUrl = parseInt(req.params.short_url);
+
+  //find the original url associated with short url
+  let urlEntry = URLDatabase.find(entry => entry.short_url == shortUrl);
+
+  if (urlEntry){
+    res.redirect(urlEntry.original_url);
+  } else{
+    res.json({ error: "No short URL found for the given input"})
+  }
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
